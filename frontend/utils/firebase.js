a9e4d1cb5c7f03f4c266b8004dca49d39b34f568
Kunal Babber <kunalbabber36@gmail.com>
2026-05-16 10:44:04 +0530

    fix: update firebase config to use kidwave project

--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -6,11 +6,12 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "auth-6c33c.firebaseapp.com",
-  projectId: "auth-6c33c",
-  storageBucket: "auth-6c33c.firebasestorage.app",
-  messagingSenderId: "982579695538",
-  appId: "1:982579695538:web:e997c944792f135d10deab"
+  authDomain: "kidwave.firebaseapp.com",
+  projectId: "kidwave",
+  storageBucket: "kidwave.firebasestorage.app",
+  messagingSenderId: "239144639739",
+  appId: "1:239144639739:web:a1998050ffaef50bcaa45e",
+  measurementId: "G-Y4CTQL9ET2"
 };
 
 // Initialize Firebase
